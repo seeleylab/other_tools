3eaa3b8973061d2852bce489c47dc7f0c0d73762
Jersey Deng <jersdeng@gmail.com>
2015-12-02 10:51:00 -0800

    changed suffix back to user-generated. made variable names clearer.

--- wmap_script.py
+++ wmap_script.py
@@ -1,13 +1,10 @@
-####################################################### w-map script 11/30/15 ########################################################################
+####################################################### w-map script 12/2/15 ########################################################################
 #DISCLAIMER: THE SEELEY LAB IS NOT RESPONSIBLE FOR ANY INACCURATE CONCLUSIONS DRAWN AS A RESULT OF USING THIS SCRIPT.
-
-#IF A DIFFERENT HC GROUP IS DESIRED, UNCOMMENT LINES 70-74 and 93 BEFORE RUNNING SCRIPT.
 
 #Import modules needed
 import os
 import glob
 import pandas
-import sys
 
 ########################################################### USER OPTIONS ###########################################################################
 #Prompt user to choose type of w-map.
@@ -31,7 +28,7 @@ if processing_type == 'FC':
 xl_f = raw_input('\n2. Enter the path of the Excel spreadsheet containing the subjects you want w-maps for.\n')
 
 while not os.path.isfile(xl_f):
-    xl_f = raw_input('Error--Specified spreadsheet is not a valid file path. Enter the path of the Excel spreadsheet containing the subjects you want W-maps for.\n')
+    xl_f = raw_input('Error--Specified spreadsheet is not a valid file path. Enter the path of the Excel spreadsheet containing the subjects you want w-maps for.\n')
 
 df = pandas.read_excel(xl_f)
 
@@ -54,85 +51,69 @@ if processing_type == 'GMA':
         else:
             print('Error--'+os.path.split(i)[0]+'/struc/SPM12_SEG_Full/smwc1* does not exist. Run segmentation and try again.')  
     print('Finished checking.')
+
+#Prompt user for the directory containing all of the HC regression model files.
+HC_model = raw_input('\n3. Enter the directory containing the HC regression model.\n')
+
+while not os.path.isdir(HC_model):
+    HC_model = raw_input('Error--Specified HC regression model is not a valid directory. Enter the directory containing the HC regression model.\n')
+
+#Prompt user for a list of covariates
+covs = eval(raw_input('\n4. Enter your covariates in the following format: [\'cov1\', \'cov2\', \'cov3\']. You MUST enter these in the same order as your beta maps from the HC regression model and in the same order as the columns in your spreadsheet.\n'))
+
+while covs != list(df.columns.values)[1:]:
+    covs = eval(raw_input('Error--Specified covariates not entered in correct format. Enter your covariates in the following format: [\'cov1\', \'cov2\', \'cov3\']. You MUST enter these in the same order as your beta maps from the HC regression model and in the same order as the columns in your spreadsheet.\n'))
     
 #Prompt user for the mask.
-mask = raw_input('\n3. Enter the path of the whole brain mask that the w-maps will be masked to.\n')
+mask = raw_input('\n5. Enter the path of the whole brain mask that the w-maps will be masked to.\n')
 
 while not os.path.isfile(mask):
     mask = raw_input('Error--Specified mask is not a valid file path. Enter the path of the mask.\n')
 
-#Set HC group.
-if processing_type == 'FC':
-    HC_model = '/data2/mridata2/Suzee/studies/W-score_multiple_regressions/rsfmri_multiple_regressions/GRN_seeds/L_PCG'
-elif processing_type == 'GMA':
-    HC_model = '/data/mridata/asias/W-score/GRNpresymp_288HC_mulreg_nocenter_withAmbi'
-
-#Prompt user for the directory containing all of the HC regression model files.
-#HC_model = raw_input('\nEnter the directory containing the HC regression model.\n')
-
-#while not os.path.isdir(HC_model):
-#    HC_model = raw_input('Error--Specified HC regression model is not a valid directory. Enter the directory containing the HC regression model.\n')
-
-#Prompt user for a list of covariates
-covs = eval(raw_input('\n4. Out of the following covariates--Age, Education, Gender, Handedness, Scanner, TIV--enter the covariates you are using in the following format: [\'cov1\', \'cov2\', \'cov3\']. \nYou MUST enter these in the same order as your beta maps from the HC regression model and in the same order as the columns in your spreadsheet.\n'))
-
-while covs != list(df.columns.values)[1:]:
-    covs = eval(raw_input('Error--Enter your covariates in the following format: [\'cov1\', \'cov2\', \'cov3\']. You MUST enter these in the same order as your beta maps from the HC regression model and in the same order as the columns in your spreadsheet.\n'))
-
-covs_suffixes = {'Age': 'a', 'Education': 'e', 'Gender': 'g', 'Handedness': 'h', 'Scanner': 's', 'TIV': 't'}
-
-suffix = ''
-
-for c in sorted(covs):
-    if c not in covs_suffixes:
-        print('\nSorry, the covariate %s is not supported yet.'%(c))
-        sys.exit(1)
-    else:
-        suffix += covs_suffixes[c]
-
-#suffix = raw_input('\n6. Enter a concise descriptive suffix for your w-map analysis results folders. Do not use spaces.\n')
+#Prompt user for a suffix which will be appended to all results folder names.
+suffix = raw_input('\n6. Enter a concise descriptive suffix for your w-map analysis results folders. Do not use spaces. (e.g. 12GRNps_vs_120HC)\n')
 
 ########################################################### CALCULATIONS ###########################################################################
-os.system('fslmaths '+HC_model+'/ResMS.nii -sqrt '+HC_model+'/sqrt_res')        #Calculate denominator for all subjects (HC regr model residuals SD map)
+os.system('fslmaths '+HC_model+'/ResMS.nii -sqrt '+HC_model+'/sqrt_res')       #Calculate denominator for all subjects (HC regr model residuals SD map)
 denominator = HC_model+'/sqrt_res'
 
 for index,row in df.iterrows():         #Loop through each subject and define paths for FC and GMA options. Then...
     subj = row  
     if processing_type == 'FC':
         wmapdir = subj['subjdir']+'/'+processedfmri_folder+'/'+seed_folder+'/wmap_'+suffix
-        actualimage = subj['subjdir']+'/'+processedfmri_folder+'/'+seed_folder+'/con_0001.nii'
+        actual_map = subj['subjdir']+'/'+processedfmri_folder+'/'+seed_folder+'/con_0001.nii'
     elif processing_type == 'GMA':
         wmapdir = os.path.split(subj['subjdir'])[0]+'/struc/SPM12_SEG_Full/wmap_'+suffix
-        actualimage = glob.glob(os.path.split(subj['subjdir'])[0]+'/struc/SPM12_SEG_Full/smwc1*')[0]
+        actual_map = glob.glob(os.path.split(subj['subjdir'])[0]+'/struc/SPM12_SEG_Full/smwc1*')[0]
 
     if os.path.exists(wmapdir):                     #...check if they have already been run. Skip if they have, or else...
         print(os.path.split(subj['subjdir'])[0]+' has already been run! Will be skipped.')
     else:
-        os.system('mkdir '+wmapdir)                 #...create a "wmap" folder for each subject to catch output
-        os.chdir(wmapdir); f = open('log', 'w')     #...initiate a log file in each subject's "wmap" folder
-        map_pred_for_subj = wmapdir+'/map_pred_for_subj'    #...calculate pred covariate values
-        cov_images = []
+        os.system('mkdir '+wmapdir)                         #...create a "wmap" folder for each subject to catch output
+        os.chdir(wmapdir); f = open('log', 'w')             #...open a log file in each subject's "wmap" folder
+        map_pred_for_subj = wmapdir+'/map_pred_for_subj'    #...define path for map predicted for subject 
+        predmaps_for_covs_list = []                         #...calculate each subject's predicted maps for each covariate
         for j in range(1, len(covs)+1):
-            cov_for_HC = HC_model+'/beta_000'+str(j+1)+'.nii'
+            beta_map = HC_model+'/beta_000'+str(j+1)+'.nii'
             subj_value = str(subj[j])
-            cov_pred_for_subj = wmapdir+'/cov_'+covs[j-1]
-            os.system('fslmaths '+cov_for_HC+' -mul '+subj_value+' '+cov_pred_for_subj)
-            cov_images.append(cov_pred_for_subj)
-            f.write('fslmaths '+cov_for_HC+' -mul '+subj_value+' '+cov_pred_for_subj+'\n\n')     #...record commands which create cov_pred images
+            predmap_for_cov = wmapdir+'/predmap_for_'+covs[j-1]
+            os.system('fslmaths '+beta_map+' -mul '+subj_value+' '+predmap_for_cov)
+            predmaps_for_covs_list.append(predmap_for_cov)
+            f.write('fslmaths '+beta_map+' -mul '+subj_value+' '+predmap_for_cov+'\n\n')    #...record commands which create pred maps for each cov
             
-        cov_images_paths = ''                                                                    #...group all cov_pred image paths together
-        for k in range(0,len(cov_images)):
-            cov_images_paths += ' -add '+cov_images[k]
+        predmaps_for_covs_str = ''                                                          #...turn predicted maps for each covariate into a string
+        for k in range(0,len(predmaps_for_covs_list)):
+            predmaps_for_covs_str += ' -add '+predmaps_for_covs_list[k]
             
-        os.system('fslmaths '+HC_model+'/beta_0001.nii'+cov_images_paths+' '+map_pred_for_subj)           #...calculate predicted map for subject
-        f.write('fslmaths '+HC_model+'/beta_0001.nii'+cov_images_paths+' '+map_pred_for_subj+'\n\n')      #...record command which creates predicted map
+        os.system('fslmaths '+HC_model+'/beta_0001.nii'+predmaps_for_covs_str+' '+map_pred_for_subj)           #...calculate map predicted for subject
+        f.write('fslmaths '+HC_model+'/beta_0001.nii'+predmaps_for_covs_str+' '+map_pred_for_subj+'\n\n')      #...record command which creates it
             
-        os.system('fslmaths '+map_pred_for_subj+' -sub '+actualimage+' '+wmapdir+'/numerator')            #...calculate numerator (predicted - observed)
+        os.system('fslmaths '+map_pred_for_subj+' -sub '+actual_map+' '+wmapdir+'/numerator')            #...calculate numerator (predicted - actual)
         numerator = wmapdir+'/numerator'
-        f.write('fslmaths '+map_pred_for_subj+' -sub '+actualimage+' '+wmapdir+'/numerator\n\n')          #...record command which creates numerator
+        f.write('fslmaths '+map_pred_for_subj+' -sub '+actual_map+' '+wmapdir+'/numerator\n\n')          #...record command which creates numerator
         
         os.system('fslmaths '+numerator+' -div '+denominator+' -mas '+mask+' '+wmapdir+'/wmap')           #...calculate w-map
         f.write('fslmaths '+numerator+' -div '+denominator+' -mas '+mask+' '+wmapdir+'/wmap')             #...record command which creates wmap
-        f.close()                                                                                         #...close each subject's log file
+        f.close()                                                                                         #...close log file
         
         print 'wmap created for '+subj[0]
